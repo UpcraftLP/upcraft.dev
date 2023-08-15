0e3ea893eb15611080286c6c69cb7e55bb8b1289
Up <upcraftedlp@gmail.com>
2023-08-15 23:59:40 +0200

    reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { Icon } from 'flowbite-svelte-icons';
-    import SocialLink from '../components/SocialLink.svelte';
-    import Meta from '../components/Meta.svelte';
+    import { Icon } from "flowbite-svelte-icons";
+    import SocialLink from "../components/SocialLink.svelte";
+    import Meta from "../components/Meta.svelte";
 </script>
 
-<Meta
-    title="Home"
-/>
+<Meta title="Home" />
 <div class="flex items-center justify-center w-full h-screen">
-    <div class="relative flex flex-col items-center justify-center w-11/12 m-auto bg-center bg-cover bg-[url('/images/hero-bg.gif')] h-[92vh] rounded-xl text-gray-50">
+    <div
+        class="relative flex flex-col items-center justify-center w-11/12 m-auto bg-center bg-cover bg-[url('/images/hero-bg.gif')] h-[92vh] rounded-xl text-gray-50"
+    >
         <div class="text-center font-gisha">
             <img
                 src="/images/profile_picture.webp"
@@ -23,16 +23,28 @@
                 <h2 class="tracking-tight">Java, C#, Web</h2>
             </div>
         </div>
-        <div class="absolute flex flex-row items-end justify-end gap-2 text-base bottom-2">
+        <div
+            class="absolute flex flex-row items-end justify-end gap-2 text-base bottom-2"
+        >
             <SocialLink name="GitHub" target="github">
-                <Icon name="github-solid" class="w-6 h-6 rounded-full hover:text-gray-950 focus:text-gray-950 hover:bg-slate-50 focus:bg-slate-50 focus:outline-none" />
+                <Icon
+                    name="github-solid"
+                    class="w-6 h-6 rounded-full hover:text-gray-950 focus:text-gray-950 hover:bg-slate-50 focus:bg-slate-50 focus:outline-none"
+                />
             </SocialLink>
             <SocialLink name="Twitter / X" target="twitter">
-                <Icon name="twitter-solid" class="w-6 h-6 hover:text-sky-400 focus:text-sky-400 focus:outline-none" />
+                <Icon
+                    name="twitter-solid"
+                    class="w-6 h-6 hover:text-sky-400 focus:text-sky-400 focus:outline-none"
+                />
             </SocialLink>
             <SocialLink name="Bluesky" target="bluesky">
                 <div class="inline-flex items-center gap-1 hover:text-sky-300">
-                    <p>Bluesky</p><Icon name="arrow-up-right-from-square-outline" class="w-4 h-4" />
+                    <p>Bluesky</p>
+                    <Icon
+                        name="arrow-up-right-from-square-outline"
+                        class="w-4 h-4"
+                    />
                 </div>
             </SocialLink>
         </div>
