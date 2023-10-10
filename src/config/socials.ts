68e6d527155290d475c30d45b24cbe67db85e0ec
Up <upcraftedlp@gmail.com>
2023-10-10 17:28:21 +0200

    add more social links

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -4,10 +4,13 @@ export interface SocialsList {
 
 export const socials: SocialsList = {
     bluesky: "https://bsky.app/profile/did:plc:77uzn2lx2zhtdgr5zfvzumji",
+    curseforge: "https://www.curseforge.com/members/upcraftlp/projects",
     //TODO discord
     github: "https://github.com/upcraftlp",
     kofi: "https://upcraft.dev/links/ko-fi",
     'ko-fi': "https://ko-fi.com/upcraftlp",
+    medium: "https://medium.com/@up_",
+    modrinth: "https://modrinth.com/user/Up",
     steam: "https://steamcommunity.com/id/upcraftlp",
     twitch: "https://twitch.tv/upcraftlp",
     twitter: "https://x.com/upcraftlp",
