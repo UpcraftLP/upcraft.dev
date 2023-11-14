39581eae37e388ab0ec2e3ee19999d0704c8a03b
Up <upcraftedlp@gmail.com>
2023-11-14 16:41:30 +0100

    use relative links for redirects

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -8,13 +8,13 @@ export const socials: SocialsList = {
     curseforge: "https://www.curseforge.com/members/upcraftlp/projects",
     //TODO discord
     github: "https://github.com/upcraftlp",
-    kofi: "https://upcraft.dev/links/ko-fi",
+    kofi: "/links/ko-fi",
     'ko-fi': "https://ko-fi.com/upcraftlp",
     medium: "https://medium.com/@up_",
     modrinth: "https://modrinth.com/user/Up",
     steam: "https://steamcommunity.com/id/upcraftlp",
     twitch: "https://twitch.tv/upcraftlp",
-    twitter: "https://x.com/upcraftlp",
+    twitter: "/links/x",
     youtube: "https://youtube.com/@upcraft_",
     x: "https://x.com/upcraftlp",
 };
