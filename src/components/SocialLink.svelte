306e59afc90416147e17cdc74e487cd47e94f21d
Up <upcraftedlp@gmail.com>
2023-11-14 16:40:28 +0100

    add class attribute to social links

--- src/components/SocialLink.svelte
+++ src/components/SocialLink.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+    import classNames from "classnames";
+
     export let target: string;
     export let name: string | undefined | null = undefined;
     export let tabindex: number | undefined = 0;
+    let className: string | undefined = undefined;
+    export { className as class };
 </script>
 
-<a href={`/links/${target}`} target="_blank" rel="noreferrer" title={name} class="group" {tabindex}>
+<a
+    href={`/links/${target}`}
+    target="_blank"
+    rel="noreferrer"
+    title={name}
+    class={classNames("group", className)}
+    {tabindex}
+>
     <slot />
 </a>
