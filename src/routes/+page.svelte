e7b972bc9322079b77a28b85ac3b99131453780c
Up <upcraftedlp@gmail.com>
2024-05-04 18:06:19 +0200

    cleanup title page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,9 +27,6 @@
                 <h1 class="text-5xl italic">Up</h1>
                 <h2 class="tracking-wide">Indie game developer</h2>
                 <h2 class="tracking-tight">Java, C#, Web</h2>
-                <h2 class="pt-2 italic">
-                    for mod commissions contact "up" on discord
-                </h2>
             </div>
             <hr class="pb-1" />
             <SocialLink name="Donate on Ko-fi" target="ko-fi">
